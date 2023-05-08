88e1110cd83935bcd1b75adbca6549bb6eb559b7
rakibsarowar <rakibsarowar@gmail.com>
2023-05-08 23:00:40 +0600

    added mongobd

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express ();
 const port = process.env.PORT || 5000;
 
@@ -7,6 +8,36 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+
+// BIjPJ1pgOdQnd2s3
+// coffeeMaster
+
+
+const uri = "mongodb+srv://coffeeMaster:BIjPJ1pgOdQnd2s3@cluster0.d94f49k.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
