4eb7667de30e52525e7b9b9c03c11767d3e2f607
rakibsarowar <rakibsarowar@gmail.com>
2023-05-10 23:04:57 +0600

    uri is successfully align with env file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express ();
 const port = process.env.PORT || 5000;
@@ -8,13 +9,17 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASS)
 
 // BIjPJ1pgOdQnd2s3
 // coffeeMaster
 
 
-const uri = "mongodb+srv://coffeeMaster:BIjPJ1pgOdQnd2s3@cluster0.d94f49k.mongodb.net/?retryWrites=true&w=majority";
+// const uri = "mongodb+srv://coffeeMaster:BIjPJ1pgOdQnd2s3@cluster0.d94f49k.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d94f49k.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
